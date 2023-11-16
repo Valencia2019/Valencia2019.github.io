6ad61dcf01b1dd768463389b2abd4414a1f12afd
wolf Valencia <valencia.mcmurray@gmail.com>
2023-11-16 17:06:30 -0500

    bits of clean up and added some missing pieces

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio'`, () => {
+/*   it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('portfolio');
@@ -23,5 +23,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
-  });
+  }); */
 });

95051316a2df0c88ca076e6240b8763b55b3007b
Valencia <valencia.mcmurray@gmail.com>
2025-05-01 13:36:13 -0400

    new ui
    need new home page next

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # My Portfolio
 Welcome to my portfolio. It is being served at: [https://valencia2019.github.io/my-portfolio/](https://valencia2019.github.io/my-portfolio/)
+
+
+
+# To Run locally
+clone repo
+open project root
+in terminal run:
+```
+npm i
+npm start
+```
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,6 @@ import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 export const routes: Routes = [
   {path: 'contact-me', component: ContactMeComponent},
-  {path: 'my-portfolio', component: AboutMeComponent},
+  {path: 'about-me', component: AboutMeComponent},
   { path: 'my-projects', component: MyProjectsComponent },
-  { path: '', redirectTo: '/my-portfolio', pathMatch: 'full' },
-  { path: '**', redirectTo: '/my-portfolio', pathMatch: 'full' }
 ];

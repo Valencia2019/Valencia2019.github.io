4f4a3b4d00cf958a0f337e225b54f43752aebe3b
Valencia McMurray <valencia.mcmurray@gmail.com>
2025-07-08 23:36:59 -0400

    Update next.config.ts
    Remove basePath and assetPrefix for GitHub Pages root deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from 'next';
 
-const isGithubPages: boolean = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig: NextConfig = {
   output: 'export',
+   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  basePath: isGithubPages ? '/Valencia2019.github.io' : '',
-  assetPrefix: isGithubPages ? '/Valencia2019.github.io/' : '',
+  }
 };
 
 export default nextConfig;

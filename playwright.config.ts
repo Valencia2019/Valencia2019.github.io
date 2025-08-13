661da1066449abe6402a6dc7cb650cab0c8e2a5b
Valencia <valencia.mcmurray@gmail.com>
2025-08-12 23:50:23 -0400

    fix env and base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,10 +22,10 @@ export default defineConfig({
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ 
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://valencia2019.github.io/' : 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
